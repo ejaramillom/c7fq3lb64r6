5c01a615adc2189f35b00d104ef47798ba9d2492
ejaramillom <ejaramilo@hotmail.com>
2020-04-11 18:29:05 -0500

    new try

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,16 +1,12 @@
 class ExpensesController < ApplicationController
   def index
-  @expenses = Expense.order("date DESC")
-  @expenses = Expense.where(user_id: current_user.id)
-  if user_signed_in?
-    if params[:concept].present?
-      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-    end
-    if params[:category_id].present?
-      @expenses = @expenses.where("category_id = ?", params[:category_id])
-    end
-  else
-    redirect_to new_user_session_path
-  end
+   @expenses = Expense.order("date DESC")
+
+   if params[:concept].present?
+		  @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+		end
+		if params[:category_id].present?
+		  @expenses = @expenses.where("category_id = ?", params[:category_id])
+		end 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,24 +23,10 @@ ActiveRecord::Schema.define(version: 2020_02_07_022521) do
     t.date "date"
     t.string "concept"
     t.decimal "amount"
-    t.integer  "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_expenses_on_category_id"
-    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "expenses", "categories", "users"
+  add_foreign_key "expenses", "categories"
 end
